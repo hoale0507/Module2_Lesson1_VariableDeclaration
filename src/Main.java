d9d6fbdad87d155e4f86491585f08e59c7eda6f8
hoale0507 <hoale090293@gmail.com>
2022-01-04 12:14:04 +0700

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    static double d1 = 20.5d;
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        long l = 1000L;
+        System.out.println("i is " + i);
+        System.out.println("f is " + f);
+        System.out.println("d is " + d);
+        System.out.println("b is " + b);
+        System.out.println("c is " + c);
+        System.out.println("s is " + s);
+        System.out.println("d1 is " + d1);
+        System.out.println("l is " + l);
+    }
+}
